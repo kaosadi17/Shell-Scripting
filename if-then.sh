baae76fa90fc792021e1308e0a433659ea7452ad
amalio <kaosadi17@gmail.com>
2018-03-19 16:05:09 +0530

    added else statement

--- if-then.sh
+++ if-then.sh
@@ -2,6 +2,8 @@ echo "please enter source and target file names"
 read src target
 if mv $src $target
 then
-echo "Your file successfully moved"
+echo "Your file successfully renamed"
+else
+echo "error in renaming file"
 fi
 
